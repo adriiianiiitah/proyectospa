9b03271f63ae7f5f4eb9edf10db6e0b403cf6900
adriiianiiitaj <adriana.love.21@gmail.com>
2015-04-20 22:10:36 -0500

    Panel usuario

--- js/vendor/devoops.js
+++ js/vendor/devoops.js
@@ -13,7 +13,7 @@
 function LoadCalendarScript(callback){
 	function LoadFullCalendarScript(){
 		if(!$.fn.fullCalendar){
-			$.getScript('plugins/fullcalendar/fullcalendar.js', callback);
+			$.getScript('../js/vendor/fullcalendar.js', callback);
 		}
 		else {
 			if (callback && typeof(callback) === "function") {
@@ -22,7 +22,7 @@ function LoadCalendarScript(callback){
 		}
 	}
 	if (!$.fn.moment){
-		$.getScript('plugins/moment/moment.min.js', LoadFullCalendarScript);
+		$.getScript('../js/vendor/moment.min.js', LoadFullCalendarScript);
 	}
 	else {
 		LoadFullCalendarScript();
---
+++ php/info-cuenta.php
@@ -0,0 +1,6 @@
+<?php
+    require_once("../vistas/navegacion-panel.html");
+    require_once("../vistas/menu-lateral.html");
+    require_once("../vistas/info-cuenta.html");
+    //require_once("../vistas/pie.html");
+?> 
