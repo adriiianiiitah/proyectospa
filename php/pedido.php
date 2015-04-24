dfedce53574093673aaa887c46f77973f21bf118
adriiianiiitaj <adriana.love.21@gmail.com>
2015-04-23 23:11:21 -0500

    Vista pedido con bootstrap

--- php/pedido.php
+++ php/pedido.php
@@ -1,6 +1,6 @@
 <?php
     require_once("../vistas/navegacion-panel.html");
     require_once("../vistas/menu-lateral.html");
-    require_once("../vistas/pedido.html");
+    //require_once("../vistas/pedido.html");
     //require_once("../vistas/pie.html");
 ?> 
