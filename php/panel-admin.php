eb84c1a700613eaeeaecac3294663588e302bcfa
adriiianiiitaj <adriana.love.21@gmail.com>
2015-04-24 21:56:14 -0500

    Diseño Responsivo

--- php/panel-admin.php
+++ php/panel-admin.php
@@ -1,5 +1,5 @@
 <?php
     require_once("../vistas/navegacion-panel.html");
-    require_once("../vistas/panel-usuario.html");
+    require_once("../vistas/panel-administracion.html");
     //require_once("../vistas/pie.html");
 ?> 
